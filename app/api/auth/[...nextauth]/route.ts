351628f5fb9194bf43bb8e06327cb69b84582aea
陈海连 <948722262@qq.com>
2025-08-26 03:49:32 +0800

    使用CredentialsProvider进行身份验证

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -8,12 +8,36 @@ export const runtime = "nodejs";
 
 import NextAuth, { NextAuthOptions } from "next-auth";
 import Google from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prisma } from "@/prisma/client";
-
+import bcrypt from "bcrypt";
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      //用户名和密码进行身份验证
+      credentials: {
+        email: { label: "Email", type: "email", placeholder: "Email" },
+        password: { label: "Password", type: "password" ,placeholder: "Password" },
+      },
+      //异步验证函数
+      async authorize(credentials, req) {
+        if (!credentials?.email || !credentials.password) return null;
+        //在数据库中通过此电子邮件查找该用户
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
+        if (!user) return null;
+        //检查密码是否匹配
+        const passwordsMatch = await bcrypt.compare(
+          credentials.password,
+          user.hashedPassword!
+        );
+        return passwordsMatch ? user : null;
+      },
+    }),
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
