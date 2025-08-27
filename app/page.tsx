462d3dd5122a8489c141f385c3710c9fe7e657e9
陈海连 <948722262@qq.com>
2025-08-27 19:45:41 +0800

    10-Optimizing 使用Next.js中的图像组件来优化图像

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,23 @@
 import ProductCard from "./components/ProductCard";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
+import Image from "next/image";
 export default async function Home() {
-  const session = await getServerSession(authOptions)
+  const session = await getServerSession(authOptions);
   return (
-    <main>
+    <main className="relative h-screen">
       <h1>hello {session && <span>{session.user?.name}</span>}</h1>
-      <ProductCard/>
+      <ProductCard />
+      <Image
+        src="https://assets.imgix.net/blog/unsplash-kiss.jpg"
+        alt="Test image"
+        fill
+        className="object-cover"
+        sizes="(max-width:480px) 100vw,(max-width:780px) 50vw,33vw"
+        unoptimized
+        quality={100}
+        priority
+      ></Image>
     </main>
   );
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "assets.imgix.net",
+        pathname: "/**",// 允许该域名下的所有路径
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
